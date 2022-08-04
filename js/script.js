b46f3198a0ace7beab1a0770405153667850b7b0
serhii-sultanov <sshsultanov@gmail.com>
2022-08-04 23:47:41 +0300

    added function sumTwoSmallestNumbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// // 1
+// function sumTwoSmallestNumbers(...array) {
+//   if (array.length < 2) {
+//     console.error("Please, enter two numbers or more");
+//     return;
+//   }
+//   const sortedArray = array.sort((a, b) => a - b);
+//   return sortedArray[0] + sortedArray[1];
+// }
+
+// console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
+// console.log(sumTwoSmallestNumbers(1, 3, 2)); // 3
+// console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
+
+// 2
+const calc = createCalculator(10);
